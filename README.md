609ca56ca129e380e626598378877c03fac4aa16
Jack Woodrow <jackwoodrow99@gmail.com>
2023-12-04 00:11:23 -0500

    Read arguments

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # minigrep
 
-Minimalistic implementation of grep in Rust.
+This project is based on [The Rust Book: Section 12](https://doc.rust-lang.org/book/ch12-00-an-io-project.html) project. Which is a minimal implementation of the grep CLI tool.
+
+## To run the application
+
+``` bash
+cargo run -- searchstring example-filename.txt
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1) {
+        Some(value) => println!("Searching for {}", value),
+        None => println!("No search string."),
+    }
+
+    match args.get(2) {
+        Some(value) => println!("In file {}", value),
+        None => println!("No file given."),
+    }
 }
