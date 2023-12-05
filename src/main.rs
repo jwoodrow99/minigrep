5f8c88caba26f418c783fdfdf92c239e387392dc
Jack Woodrow <jackwoodrow99@gmail.com>
2023-12-04 23:43:50 -0500

    12.3 Creating a Constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config: Config = parse_config(&args);
+    let config = Config::new(&args);
 
     match fs::read_to_string(config.file_path) {
         Ok(contents) => {
@@ -18,16 +18,18 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query: String = match &args.get(1) {
-        Some(value) => value.to_string(),
-        None => panic!("No search query provided."),
-    };
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query: String = match &args.get(1) {
+            Some(value) => value.to_string(),
+            None => panic!("No search query provided."),
+        };
 
-    let file_path: String = match &args.get(2) {
-        Some(value) => value.to_string(),
-        None => panic!("No file path provided."),
-    };
+        let file_path: String = match &args.get(2) {
+            Some(value) => value.to_string(),
+            None => panic!("No file path provided."),
+        };
 
-    return Config { query, file_path };
+        return Config { query, file_path };
+    }
 }
