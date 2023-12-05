ef08418b7232dc5e0e5fedea484851ced09103a3
Jack Woodrow <jackwoodrow99@gmail.com>
2023-12-04 23:08:38 -0500

    Read file 12.2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match args.get(1) {
-        Some(value) => println!("Searching for {}", value),
-        None => println!("No search string."),
-    }
-
     match args.get(2) {
-        Some(value) => println!("In file {}", value),
+        Some(value) => {
+            match args.get(1) {
+                Some(value) => println!("Searching for {}", value),
+                None => println!("No search string."),
+            }
+
+            match fs::read_to_string(&value) {
+                Ok(contents) => {
+                    println!("With text: \n{contents}");
+                }
+                Err(_) => println!("Cannot read this file."),
+            }
+        }
         None => println!("No file given."),
     }
 }
